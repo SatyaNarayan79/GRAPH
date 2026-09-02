5c71aef3560d6ca6b8338df039c62a3c9168b142
SatyaNarayan79 <satyanarayanbarik9966@gmail.com>
2026-09-02 22:48:56 +0530

    binarytree 1st

---
+++ BinaryTree.py
@@ -0,0 +1,35 @@
+""" 
+            drink
+         /         \
+       hot         cold
+      /   \       /     \
+  coffee   tea  cola  fanta 
+  
+  """
+ 
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+
+ 
+drink= Node("drink") 
+hot= Node("hot")
+cold= Node("cold")
+coffee= Node("coffee")
+tea= Node("tea")
+cola= Node("cola")
+fanta = Node("fanta")
+
+hot.left=coffee
+hot.right=tea
+
+cold.left=cola
+cold.right=fanta
+
+drink.left=hot
+drink.right=cold 
+
+print(drink.left) 
+print(hot.val)          
